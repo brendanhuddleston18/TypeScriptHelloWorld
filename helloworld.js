5407505b2b06f6f677d918b8e4631c4b28af6ac2
Brendan Huddleston <bhudd11@wgu.edu>
2025-05-04 21:19:32 -0500

    added some simple code to get the hang of it and knock some rust off

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,30 @@
-var message = 'Hello World';
-console.log(message);
+// let message : string = 'Hello World';
+// console.log(message);
+// function helloWorld() : string {
+//    let  helloWorld : string = 'Hello World';
+//    return helloWorld;
+// }
+// let myStatement: string = helloWorld();
+// let myArray: string[] = ['Brendan', 'Jack', 'Lucy'];
+// myArray.push('Jon');
+// console.log(myArray[3]);
+// // for (let i: number = 0; i < myArray.length; i++) {
+// //     console.log(myArray[i]);
+// // }
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+function makeNewPerson(name, age, hobbies) {
+    var person = new Person();
+    person.name = name;
+    person.age = age;
+    person.hobbies = hobbies;
+    console.log(person.name);
+    console.log(person.age);
+    for (var i = 0; i < person.hobbies.length; i++) {
+        console.log(person.hobbies[i]);
+    }
+}
+makeNewPerson('Brendan', 27, ['Photography', 'Gym', 'Golf']);
--- helloworld.ts
+++ helloworld.ts
@@ -1,2 +1,40 @@
-let message : string = 'Hello World';
-console.log(message);
+// let message : string = 'Hello World';
+// console.log(message);
+
+// function helloWorld() : string {
+//    let  helloWorld : string = 'Hello World';
+//    return helloWorld;
+// }
+
+// let myStatement: string = helloWorld();
+
+// let myArray: string[] = ['Brendan', 'Jack', 'Lucy'];
+
+// myArray.push('Jon');
+
+// console.log(myArray[3]);
+
+// // for (let i: number = 0; i < myArray.length; i++) {
+// //     console.log(myArray[i]);
+// // }
+
+class Person {
+    name: string;
+    age: number;
+    hobbies: string[];
+}
+
+function makeNewPerson(name: string, age: number, hobbies: string[]):void {
+    const person = new Person();
+    person.name = name;
+    person.age = age;
+    person.hobbies = hobbies;
+    console.log(person.name);
+    console.log(person.age);
+    for(let i = 0; i < person.hobbies.length; i++) {
+        console.log(person.hobbies[i]);
+    }
+}
+
+makeNewPerson('Brendan', 27, ['Photography', 'Gym', 'Golf']);
+
